f1d220090ea6c480dc63e0429ff51b9b0b5fe814
Peggy <peggyhuang963@gmail.com>
2026-02-06 06:15:27 +0800

    login

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,55 @@
 <script setup>
 import { RouterView } from 'vue-router';
+import { ref, watch } from 'vue';
+import { useRouter, useRoute } from 'vue-router';
+
+const router = useRouter();
+const route = useRoute();
+// 控制是否顯示登出按鈕
+const isLoggedIn = ref(false);
+
+//監聽路由變化：每次換頁時，都檢查一次口袋裡有沒有 Token
+watch(route, () =>{
+  const token = localStorage.getItem('token');
+  // !!token 是一種縮寫，把 token 轉成布林值 (有值變 true，沒值變 false)
+  isLoggedIn.value = !!token;
+});
+
+//登出功能
+const handleLogout = () =>{
+  // 1. 清除口袋裡的通行證
+  localStorage.removeItem('token');
+  localStorage.removeItem('user');
+  // 2. 更新狀態
+  isLoggedIn.value = false;
+  // 3. 踢回首頁
+  alert('已登出');
+  router.push('/');
+}
 </script>
 
-<template>
-  <nav class="bg-gray-800 text-white sticky top-0 z-10 p-4 flex gap-4">
 
-    <RouterLink to="/" class="hover:text-blue-300 transition-colors">回首頁</RouterLink>
-    <RouterLink to="/admin" class="hover:text-blue-300 transition-colors">進入後台</RouterLink>
+<template>
+  <nav class="bg-gray-800 text-white sticky top-0 z-50 py-2 h-16 flex gap-4">
+    <div class="flex justify-between items-center">
+
+      <div class="flex gap-6 items-center">
+        <h1 class="text-l font-bold m-2">My Shop</h1>
+        <RouterLink to="/" class="hover:text-blue-300 transition-colors">首頁</RouterLink>
+        <RouterLink to="/admin" class="hover:text-blue-300 transition-colors">進入後台</RouterLink>
+      </div>
+
+      <div>
+        <button v-if="isLoggedIn" @click="handleLogout" class="bg-red-500 hover:bg-red-600 px-4 py-2 m-4 rounded text-sm font-bold transition-colors">
+          登出
+        </button>
+
+        <RouterLink v-else to="/login" class="bg-red-500 hover:bg-red-600 px-4 py-2 m-4 rounded text-sm font-bold transition-colors">
+          登入
+        </RouterLink>
+      </div>
+
+    </div>  
 
   </nav>
 
@@ -15,3 +58,4 @@ import { RouterView } from 'vue-router';
   <footer class="bg-gray-800 text-white p-16 flex "> test</footer>
 
 </template>
+
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -7,6 +7,7 @@ import{createRouter, createWebHistory} from 'vue-router';
 import ShopView from '../views/ShopView.vue';
 import AdminView from '../views/AdminView.vue';
 import ProductDetail from '../views/ProductDetail.vue';
+import LoginView from '../views/LoginView.vue';
 
 // 3. 開始建立路由器
 const router = createRouter({
@@ -24,15 +25,42 @@ const router = createRouter({
         {
             path:'/admin',
             name:'admin',
-            component: AdminView
+            component: AdminView,
+            meta: {requiresAuth: true} // 貼上標籤：此頁面需要驗證
         },
         {
             path:'/product/:id',
             name:'product-detail',
             component: ProductDetail
         },
+        {
+            path:'/login',
+            name: 'login',
+            component: LoginView
+        },
     ]
 
+});
+
+// 全域路由守衛：每次換頁前，都會經過這裡
+router.beforeEach((to, from, next) => {
+    // 1. 檢查要去的地方，需不需要權限 (看有沒有 requiresAuth 標籤)
+    if(to.meta.requiresAuth){
+        // 2. 如果需要權限，檢查口袋有沒有 token
+        const token = localStorage.getItem('token');
+
+        if(token){
+            // 3. 有 token，放行！
+            next();
+        }else{
+            // 4. 沒 token，踢去登入頁
+            alert('請先登入！');
+            next('/login');
+        };
+    }else{
+        // 5. 如果要去的地方不用權限 (像首頁)，直接放行
+        next();
+    };
 });
 
 // 5. 匯出這個設定好的 router，讓 main.js 可以使用
